df06faad0e0a9c4181657cf6112efa0dea536a0f
Mustafa Alkamali <m7asanain@gmail.com>
2023-04-01 02:31:17 +0300

    javascript project: loading state

--- word-masters.js
+++ word-masters.js
@@ -1,7 +1,42 @@
 const letters = document.querySelectorAll(".scoreboard-letter");
 const loadingDiv = document.querySelector(".info-bar");
+const ANSWER_LENGTH = 5;
 
 async function init() {
+  let currentGuess = '';
+  let currentRow = 0;
+
+  function addLetter(letter) {
+    if (currentGuess.length < ANSWER_LENGTH) {
+      // add letter to the end
+      currentGuess += letter;
+    } else {
+      // replace the last letter
+      currentGuess = currentGuess.substring(0, currentGuess.length - 1) + letter;
+    }
+    letters[currentRow * ANSWER_LENGTH + currentGuess.length - 1].innerText = letter;
+  }
+
+  async function commit() {
+    if (currentGuess.length !== ANSWER_LENGTH) {
+      // do nothing
+      return;
+    }
+    
+    // TODO validate the word
+
+    // TODO do all the marking as "corrent", "close", "wrong"
+
+    // TODO did they win or lose?
+
+    currentRow++;
+    currentGuess = '';
+  }
+
+  function backspace() {
+    currentGuess = currentGuess.substring(0, currentGuess.length - 1);
+    letters[currentRow * ANSWER_LENGTH + currentGuess.length].innerText = "";
+  }
   
   document.addEventListener('keydown', function hundleKeyPress(event) {
     const action = event.key;
@@ -18,6 +53,10 @@ async function init() {
       // do nothing
     }
   });
+}
+
+function isLetter(letter) {
+  return /^[a-zA-Z]$/.test(letter);
 }
 
 init();
