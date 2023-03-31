1e56597baad4befe41608174bd975bc07dbf5e5f
Mustafa Alkamali <m7asanain@gmail.com>
2023-04-01 02:52:39 +0300

    javascript project: loading state

--- word-masters.js
+++ word-masters.js
@@ -6,6 +6,13 @@ async function init() {
   let currentGuess = '';
   let currentRow = 0;
 
+  const res = await fetch("https://words.dev-apis.com/word-of-the-day");
+  const resObj = await res.json();
+  const word = resObj.word.toUpperCase();
+  setLoading(false);
+
+  console.log(word);
+
   function addLetter(letter) {
     if (currentGuess.length < ANSWER_LENGTH) {
       // add letter to the end
@@ -57,6 +64,10 @@ async function init() {
 
 function isLetter(letter) {
   return /^[a-zA-Z]$/.test(letter);
+}
+
+function setLoading(isLoding) {
+  loadingDiv.classList.toggle('show', isLoding);
 }
 
 init();
